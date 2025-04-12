739400705cbd91d45a5b688df0a1493f19977898
d3ltaaa <hil.falk@protonmail.com>
2025-04-12 22:57:34 +0200

    hypridle_icon

--- hypridle_icon.sh
+++ hypridle_icon.sh
@@ -1,6 +1,7 @@
 icon_idle=""
 if [[ $(systemctl --user is-active hypridle.service) == "active" ]]; then
-  systemctl --user stop hypridle.service
+  icon_idle="󱣴"
 else
-  systemctl --user start hypridle.service
+  icon_idle=""
 fi
+echo "{\"text\": \" $icon_idle \" }"
