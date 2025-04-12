e06e6605656855a3bd08384fb41f108710bd1fd6
d3ltaaa <hil.falk@protonmail.com>
2025-04-13 00:02:42 +0200

    fix2

--- toggle_hypridle.sh
+++ toggle_hypridle.sh
@@ -1,6 +1,6 @@
-icon_idle=""
 if [[ $(systemctl --user is-active hypridle.service) == "active" ]]; then
   systemctl --user stop hypridle.service
 else
   systemctl --user start hypridle.service
 fi
+pkill -RTMIN+6 waybar
