872f7faef6a6fddea99246ce187ef804f4b692e1
d3ltaaa <hil.falk@protonmail.com>
2025-04-17 22:03:25 +0200

    screenshots

--- screenshot_save.sh
+++ screenshot_save.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
 # menu="dmenu -i"
 menu="rofi -dmenu -i"
-DIR="/home/falk/Pictures/Screenshots"
+XDG_SCREENSHOT_DIR="/home/falk/Pictures/Screenshots"
 # take screenshot and open
-grim -g "$(slurp)" "$DIR/temp.png" &&
-  feh --scale-down --auto-zoom "$DIR/temp.png" &&
+grim -g "$(slurp)" "$XDG_SCREENSHOT_DIR/temp.png" &&
+  feh --scale-down --auto-zoom "$XDG_SCREENSHOT_DIR/temp.png" &&
   selected=$(printf 'yes\nno' | $menu -p "Save?:")
 case $selected in
 "yes")
   # give name
   NAME="$($menu -p "Name:")"
-  FILE_NAME="$DIR/$NAME.png"
-  mv "$DIR/temp.png" "$FILE_NAME"
+  FILE_NAME="$XDG_SCREENSHOT_DIR/$NAME.png"
+  mv "$XDG_SCREENSHOT_DIR/temp.png" "$FILE_NAME"
   exit 0
   ;;
 
 "*") ;;
 esac
 # delete if not yes (also if menu is cancelled)
-rm "$DIR/temp.png"
+rm "$XDG_SCREENSHOT_DIR/temp.png"
