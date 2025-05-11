3e10f3ec5cdab4bcee247392a3279d73322a67df
d3ltaaa <hil.falk@protonmail.com>
2025-05-11 20:34:48 +0200

    tooltip

--- hypridle_icon.sh
+++ hypridle_icon.sh
@@ -1,7 +1,10 @@
 icon_idle=""
+tooltip_text=""
 if [[ $(systemctl --user is-active hypridle.service) == "active" ]]; then
   icon_idle=" "
+  tooltip_text="hypridle active"
 else
   icon_idle="󱣴 "
+  tooltip_text="hypridle inactive"
 fi
-echo "{\"text\": \"$icon_idle\" }"
+echo "{\"text\": \"$icon_idle\", \"tooltip\": \"$tooltip_text\" }"
--- vpn_icon.sh
+++ vpn_icon.sh
@@ -1,9 +1,12 @@
 icon_vpn=""
+tooltip_text=""
 if [[ $(systemctl is-enabled wg-quick-wg0.service) == "enabled" ]]; then
   if [[ $(systemctl is-active wg-quick-wg0.service) == "active" ]]; then
     icon_vpn="󰌘 "
+    tooltip_text="VPN connected"
   else
     icon_vpn="󰌙 "
+    tooltip_text="VPN disconnected"
   fi
-  echo "{\"text\": \"$icon_vpn\" }"
+  echo "{\"text\": \"$icon_vpn\", \"tooltip\": \"$tooltip_text\" }"
 fi
