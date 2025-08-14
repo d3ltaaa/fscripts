5cdbd4fd69aef670799e8570f11277bc745354d8
d3ltaaa <hil.falk@protonmail.com>
2025-08-14 13:39:57 +0200

    vpn

--- toggle_vpn.sh
+++ toggle_vpn.sh
@@ -1,6 +1,6 @@
 if [[ $(systemctl is-active wg-quick-wg0.service) == "active" ]]; then
-  foot -e systemctl stop wg-quick-wg0.service
+  pkexec systemctl stop wg-quick-wg0.service
 else
-  foot -e systemctl start wg-quick-wg0.service
+  pkexec systemctl start wg-quick-wg0.service
 fi
 pkill -RTMIN+7 waybar
