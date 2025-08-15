8e754e6cc139b9f43580cfac9400f3845a080239
d3ltaaa <hil.falk@protonmail.com>
2025-08-15 10:20:44 +0200

    vpn

--- vpn_icon.sh
+++ vpn_icon.sh
@@ -1,6 +1,9 @@
 icon_vpn=""
 tooltip_text=""
 if [[ $(systemctl is-enabled wg-quick-wg0.service) == "enabled" ]]; then
+  ping -c 1 "1.1.1.1" > /dev/null 2>&1
+  internet_status=$?
+
   if [[ $(systemctl is-active wg-quick-wg0.service) == "active" ]]; then
     icon_vpn="󰌘 "
     tooltip_text="VPN connected"
@@ -8,5 +11,12 @@ if [[ $(systemctl is-enabled wg-quick-wg0.service) == "enabled" ]]; then
     icon_vpn="󰌙 "
     tooltip_text="VPN disconnected"
   fi
-  echo "{\"text\": \"$icon_vpn\", \"tooltip\": \"$tooltip_text\" }"
+
+  if [ $internet_status -eq 0 ]; then
+    internet_text=""
+  else
+    internet_text=""
+  fi
+
+  echo "{\"text\": \"$icon_vpn - $internet_text\", \"tooltip\": \"$tooltip_text\" }"
 fi
