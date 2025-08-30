728ad25d47810e29c6638133dda26e05532a3b4a
d3ltaaaaaa <hil.falk@protonmail.com>
2025-08-30 15:40:00 +0200

    idk

--- test_combined.sh
+++ test_combined.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/run/current-system/sw/bin/bash
 
 st -e test_battery.sh  &
 
 firefox https://www.youtube.com/watch?v=jfKfPfyJRdk  &
 
-st -e test_compress.sh 
+st -e test_compress.sh
 
