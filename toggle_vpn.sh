5d5ff933e35d1322ea1e6215674f978c00ef9367
d3ltaaa <hil.falk@protonmail.com>
2025-10-07 22:40:27 +0200

    pkexec rm

--- toggle_vpn.sh
+++ toggle_vpn.sh
@@ -1,6 +1,7 @@
+terminal="foot -e"
 if [[ $(systemctl is-active wg-quick-wg0.service) == "active" ]]; then
-  pkexec systemctl stop wg-quick-wg0.service
+  hyprctl dispatch exec "[float;size 800 500] $terminal sudo systemctl stop wg-quick-wg0.service"
 else
-  pkexec systemctl start wg-quick-wg0.service
+  hyprctl dispatch exec "[float;size 800 500] $terminal sudo systemctl start wg-quick-wg0.service"
 fi
 pkill -RTMIN+7 waybar
